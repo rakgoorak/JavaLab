3af46a8d9bac60830e90b87278db2d495eaf6512
rakgoorak <30443791+rakgoorak@users.noreply.github.com>
2023-07-04 23:35:41 +0700

    LabJava
    come on!!!

---
+++ BaseJava.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class BaseJava {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+    }
+}
